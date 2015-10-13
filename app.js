21edfb784e46ad300058d2945f7a7f0776422726
jotaoncode <garcia_juan_j@yahoo.com.ar>
2015-10-13 11:26:29 -0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,10 @@
+var ViewModel = function (first, last) {
+  this.firstName = ko.observable(first);
+  this.lastName = ko.observable(last);
+
+  this.fullName = ko.pureComputed(function () {
+    return this.firstName() + " " + this.lastName();
+  }, this);
+};
+
+ko.applyBindings(new ViewModel("Planet", "Earth"));
