637bd696d74c1ace0a6c0647e2e1ef1c63286af1
jotaoncode <garcia_juan_j@yahoo.com.ar>
2015-10-14 15:36:17 -0300

    Added some markers to my neighbourhood map.

--- app.js
+++ app.js
@@ -1,14 +1,79 @@
-var ViewModel = function (first, last) {
-  this.firstName = ko.observable(first);
-  this.lastName = ko.observable(last);
+var app = (function () {
+  var map,
+  markers = [],
+  markersDescription =  {
+    home: {
+      pos: {
+        lat: -34.6057242,
+        lng: -58.415755399999966
+      },
+      description: 'Home'
+    },
+    casaDelQueso: {
+      pos: {
+        lat: -34.603760,
+        lng: -58.415243
+      },
+      description: 'Casa del Queso'
+    },
+    tierraSanta: {
+      pos: {
+        lat: -34.609030,
+        lng: -58.414730
+      },
+      description: 'Tierra Santa'
+    },
+    pioIx: {
+      pos: {
+        lat: -34.614650,
+        lng: -58.421704
+      },
+      description: 'Pio IX'
+    },
+    utn: {
+      pos: {
+        lat: -34.598584,
+        lng: -58.419998
+      },
+      description: 'UTN'
+    },
+    sanataBar: {
+      pos: {
+        lat: -34.605756,
+        lng: -58.415116
+      },
+      description: 'Sanata Bar'
+    }
+  };
+  var FilterList = function (places) {
+    var self = this;
+    this.place = ko.observableArray(places);
+    this.currentSearch = ko.computed(function (textSearch) {
+      console.log('write current search', arguments);
+      return textSearch;
+    }, this);
+    this.setMarkerRenderedInMap = function (place) {
+      this.place.push(place);
+    };
+  };
+  var filterList = new FilterList(markers);
+  ko.applyBindings(filterList);
+  function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+      center: markersDescription.home.pos,
+      zoom: 15
+    });
+    for (var name in markersDescription) {
+      filterList.setMarkerRenderedInMap(new google.maps.Marker({
+        map: map,
+        position: markersDescription[name].pos,
+        title: markersDescription[name].description
+      }));
+    }
+  }
 
-  this.fullName = ko.pureComputed(function () {
-    return this.firstName() + " " + this.lastName();
-  }, this);
-};
+  return {
+    initMap: initMap
+  };
+}());
 
-ko.applyBindings(new ViewModel("Planet", "Earth"));
-
-function initMap() {
-  console.log(arguments);
-}
