3d31a38f284538bdcbeabd35f40f9fa0c0c4cff7
jotaoncode <garcia_juan_j@yahoo.com.ar>
2015-10-15 08:51:52 -0300

    Refactor code, in order to separete responsabilities accros a model a viewmodel and the view.

--- app.js
+++ app.js
@@ -1,70 +1,96 @@
 var app = (function () {
   var map,
-  markers = [],
-  markersDescription =  {
-    home: {
-      pos: {
-        lat: -34.6057242,
-        lng: -58.415755399999966
+    listModel =  {
+      markersDefinition : {
+        home: {
+          pos: {
+            lat: -34.6057242,
+            lng: -58.415755399999966
+          },
+          description: 'Home'
+        },
+        casaDelQueso: {
+          pos: {
+            lat: -34.603760,
+            lng: -58.415243
+          },
+          description: 'Casa del Queso'
+        },
+        tierraSanta: {
+          pos: {
+            lat: -34.609030,
+            lng: -58.414730
+          },
+          description: 'Tierra Santa'
+        },
+        pioIx: {
+          pos: {
+            lat: -34.614650,
+            lng: -58.421704
+          },
+          description: 'Pio IX'
+        },
+        utn: {
+          pos: {
+            lat: -34.598584,
+            lng: -58.419998
+          },
+          description: 'UTN'
+        },
+        sanataBar: {
+          pos: {
+            lat: -34.605756,
+            lng: -58.415116
+          },
+          description: 'Sanata Bar'
+        }
       },
-      description: 'Home'
-    },
-    casaDelQueso: {
-      pos: {
-        lat: -34.603760,
-        lng: -58.415243
-      },
-      description: 'Casa del Queso'
-    },
-    tierraSanta: {
-      pos: {
-        lat: -34.609030,
-        lng: -58.414730
-      },
-      description: 'Tierra Santa'
-    },
-    pioIx: {
-      pos: {
-        lat: -34.614650,
-        lng: -58.421704
-      },
-      description: 'Pio IX'
-    },
-    utn: {
-      pos: {
-        lat: -34.598584,
-        lng: -58.419998
-      },
-      description: 'UTN'
-    },
-    sanataBar: {
-      pos: {
-        lat: -34.605756,
-        lng: -58.415116
-      },
-      description: 'Sanata Bar'
-    }
-  };
-  var FilterList = function (places) {
-    var self = this;
+      markersInstance: []
+    };
+
+  var FilterListViewModel = function (places) {
+    var self = this, titlesContainer = $('#places');
     this.place = ko.observableArray(places);
     this.searchText = ko.observable("");
-    this.filterMarkers = function (criteria) {
-      if (criteria === "") {
-        _.each(markers, function (marker) { marker.setVisible(true); });
-        _.each(this.place, function (place) {
-          $(place).show();
-        });
-        return;
-      }
-      var successList = _.filter(markers, function (marker) {
+
+    this.findTitleInList = function (marker) {
+      var title = _.find(titlesContainer.find('li'), function (title) {
+        return $(title).text() === marker.title;
+      });
+      return $(title);
+    };
+    this.getMarkersMatchCriteria = function (criteria) {
+      return _.filter(listModel.markersInstance, function (marker) {
         return marker.title.toLowerCase().indexOf(criteria.toLowerCase()) >= 0;
       });
-
-      var failList = _.difference(markers, successList);
-      _.each(successList, function (marker) { marker.setVisible(true); });
-      _.each(failList, function (marker) { marker.setVisible(false); });
     };
+    this.setVisibilityToList = function (list, bolean) {
+      _.each(list, function (marker) {
+        marker.setVisible(bolean);
+        if (bolean) {
+          self.findTitleInList(marker).show();
+        } else {
+          self.findTitleInList(marker).hide();
+        }
+      });
+    };
+    this.filterMarkers = function (criteria) {
+      var titles = titlesContainer.find('li'),
+          successList,
+          failList;
+
+      if (criteria === "") {
+        this.setVisibilityToList(listModel.markersInstance, true);
+        return;
+      }
+
+      successList = this.getMarkersMatchCriteria(criteria);
+      failList = _.difference(listModel.markersInstance, successList);
+
+      this.setVisibilityToList(successList, true);
+      this.setVisibilityToList(failList, false);
+    };
+
     this.currentSearch = ko.computed({
       read: function () {
         this.filterMarkers(this.searchText());
@@ -72,25 +98,27 @@ var app = (function () {
       },
       owner: this
     });
+
     this.setMarkerRenderedInMap = function (place) {
       this.place.push(place);
     };
   };
-  var filterList = new FilterList(markers);
-  ko.applyBindings(filterList);
+
   function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
-      center: markersDescription.home.pos,
+      center: listModel.markersDefinition.home.pos,
       zoom: 15
     });
-    for (var name in markersDescription) {
+    _.each(listModel.markersDefinition, function (marker) {
       filterList.setMarkerRenderedInMap(new google.maps.Marker({
         map: map,
-        position: markersDescription[name].pos,
-        title: markersDescription[name].description
+        position: marker.pos,
+        title: marker.description
       }));
-    }
+    });
   }
+  var filterList = new FilterListViewModel(listModel.markersInstance);
+  ko.applyBindings(filterList);
   return {
     initMap: initMap
   };
