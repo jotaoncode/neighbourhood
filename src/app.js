6e24c1e73a59b620377a0c3959e9deb4d10c4807
jotaoncode <garcia_juan_j@yahoo.com.ar>
2015-10-21 23:47:49 -0300

    Added an api file to get values from app server.

--- src/app.js
+++ src/app.js
@@ -259,39 +259,4 @@ var app = (function () {
   };
 }());
 
-function getVenue() {
-  var data = {
-    positions: 'll=40.7,-74'
-  };
-  $.ajax({
-    url: 'http://localhost:3000/foursquare/venues',
-    type: 'GET',
-    data: data,
-  }).then(function () {
-    console.log('finished: ', arguments);
-  });
-}
-function getTip() {
-  var data = {
-    tip_id: '4eaf024f9a521bacdb216401'
-  };
-  $.ajax({
-    url: 'http://localhost:3000/foursquare/tip',
-    type: 'GET',
-    data: data,
-  }).then(function () {
-    console.log('finished: ', arguments);
-  });
-}
-function getTips() {
-  var data = {
-    venue_id: '430d0a00f964a5203e271fe3'
-  };
-  $.ajax({
-    url: 'http://localhost:3000/foursquare/tips',
-    method: 'GET',
-    data: data,
-  }).then(function () {
-    console.log('finished: ', arguments);
-  });
-}
+
